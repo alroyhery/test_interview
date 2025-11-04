aa0babc67d6026e261a13afdb6c2100b7741db7c
alroyhery <90199997+alroyhery@users.noreply.github.com>
2025-11-04 17:48:11 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
-// db.js
 const mysql = require('mysql2/promise');
-// require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root',
-  database: process.env.DB_NAME || 'inter',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
