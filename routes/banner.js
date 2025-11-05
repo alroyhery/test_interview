128e962fb8532cdf4bfff29b3c69d6d5537ddea7
alroyhery <90199997+alroyhery@users.noreply.github.com>
2025-11-05 07:21:29 +0700

    Update banner.js

--- routes/banner.js
+++ routes/banner.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db'); // sesuaikan path
+const pool = require('../db');
 
 router.get('/banner', async (req, res) => {
   try {
